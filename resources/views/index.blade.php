6b0bb394eebab5ee80fe5a03793c983b6defcc39
igorliotta <liottaigor@gmail.com>
2023-12-15 17:10:22 +0100

    ADD - view and style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,34 @@
 
 @section('content')
     {{-- <h1>Homepage</h1> --}}
-    <h1>Scegli il tuo personaggio</h1>
-    @forelse ($characters as $character)
+    <h1 class="text-center p-5">Scegli il tuo personaggio</h1>
+
        <section>
         <div class="container">
-            
+            <table class="table table-primary">
+                <thead>
+                    <tr>
+                        <th scope="col">Livello</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Difesa</th>
+                        <th scope="col">Velocità</th>
+                        <th scope="col">Salute</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($characters as $character)
+                    <tr>
+                        <td>{{$character->level}}</td>
+                         <td>{{$character->name}}</td>
+                        <td>{{$character->bio}}</td>
+                       <td>{{$character->defence}}</td>
+                       <td>{{$character->speed}}</td>
+                       <td>{{$character->hp}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
        </section>
-    @empty
-        
-    @endforelse
 @endsection
